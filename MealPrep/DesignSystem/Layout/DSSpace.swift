cedd1f4a2a89b63d018fc4c3ff8ea2bc6559c537
Dmytro Ipatii <dima.ipatjj@gmail.com>
2026-09-05 14:26:40 +0200

    Created layout value tokens

---
+++ MealPrep/DesignSystem/Layout/DSSpace.swift
@@ -0,0 +1,27 @@
+//
+//  DSSpace.swift
+//  MealPrep
+//
+//  Created by Dmytro Ipatii on 05/09/2026.
+//
+
+import Foundation
+
+enum DSSpace {
+    case none
+    case sm
+    case md
+    case lg
+    case xl
+
+    var value: CGFloat {
+        switch  self {
+        case .none: 0
+        case .sm: 12
+        case .md: 16
+        case .lg: 20
+        case .xl: 32
+        }
+    }
+
+}
